241d2783a07232921f0cef3b342f6ca01a45001f
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2014-05-23 11:26:34 +0200

    Add turntable manipulator.

--- source/example.cpp
+++ source/example.cpp
@@ -12,6 +12,8 @@
 #include <fstream>
 #include <algorithm>
 #include <stdexcept>
+#define _USE_MATH_DEFINES
+#include <cmath>
 #define GLM_FORCE_RADIANS
 
 ///GLM INCLUDES
@@ -22,9 +24,10 @@
 #include <glm/gtx/norm.hpp>
 
 ///PROJECT INCLUDES
-#include "volume_loader_raw.hpp"
-#include "transfer_function.hpp"
-#include "utils.hpp"
+#include <volume_loader_raw.hpp>
+#include <transfer_function.hpp>
+#include <utils.hpp>
+#include <turntable.hpp>
 
 const std::string g_file_vertex_shader("../../../data/volume.vert");
 const std::string g_file_fragment_shader("../../../data/volume.frag");
@@ -39,6 +42,10 @@ GLuint loadShaders(std::string const& vs, std::string const& fs)
 bool g_reload_shader_pressed                = false;
 bool g_show_transfer_function               = false;
 bool g_show_transfer_function_pressed       = false;
+glm::ivec3 g_mouse_button_pressed{0,0,0};
+glm::vec2  g_mouse{0.0f,0.0f};
+glm::vec2  g_lastMouse{0.0f,0.0f};
+Turntable  g_turntable;
 
 ///SETUP VOLUME RAYCASTER HERE
 std::string g_file_string                   = "../../../data/head_w256_h256_d225_c1_b8.raw";
@@ -46,9 +53,60 @@ float       g_sampling_distance             = 0.001f;
 glm::vec3   g_light_pos                     = glm::vec3(10.0, 10.0, 0.0);
 glm::vec3   g_light_color                   = glm::vec3(1.0f, 1.0f, 1.0f);
 
+struct Manipulator
+{
+  Manipulator()
+    : m_mouse_button_pressed{0,0,0}
+    , m_mouse{0.0f,0.0f}
+    , m_lastMouse{0.0f,0.0f}
+    , m_turntable{}
+    {}
+
+  glm::mat4 matrix(Window const& win)
+  {
+    m_mouse = win.mousePosition();
+    if (win.isButtonPressed(Window::MOUSE_BUTTON_LEFT)) {
+      if (!m_mouse_button_pressed[0]) {
+        m_mouse_button_pressed[0] = 1;
+      }
+      m_turntable.rotate(m_lastMouse, m_mouse);
+    } else {
+      m_mouse_button_pressed[0] = 0;
+    }
+
+    if (win.isButtonPressed(Window::MOUSE_BUTTON_MIDDLE)) {
+      if (!m_mouse_button_pressed[1]) {
+        m_mouse_button_pressed[1] = 1;
+      }
+      m_turntable.pan(m_lastMouse, m_mouse);
+    } else {
+      m_mouse_button_pressed[1] = 0;
+    }
+
+    if (win.isButtonPressed(Window::MOUSE_BUTTON_RIGHT)) {
+      if (!m_mouse_button_pressed[2]) {
+        m_mouse_button_pressed[2] = 1;
+      }
+      m_turntable.zoom(m_lastMouse, m_mouse);
+    } else {
+      m_mouse_button_pressed[2] = 0;
+    }
+
+    m_lastMouse = m_mouse;
+    return m_turntable.matrix();
+  }
+
+  glm::ivec3 m_mouse_button_pressed;
+  glm::vec2  m_mouse;
+  glm::vec2  m_lastMouse;
+  Turntable  m_turntable;
+};
+
 int main(int argc, char* argv[])
 {
   Window win(glm::ivec2(1200,800));
+
+  Manipulator manipulator;
 
   ///SETTING TRANSFERFUNCTION HERE
   Transfer_function transfer_fun;
@@ -91,7 +149,7 @@ int main(int argc, char* argv[])
     }
 
     if (win.isKeyPressed(GLFW_KEY_R)) {
-        if (g_reload_shader_pressed != true){
+        if (g_reload_shader_pressed != true) {
             GLuint newProgram(0);
             try {
                 std::cout << "Reload shaders" << std::endl;
@@ -120,7 +178,6 @@ int main(int argc, char* argv[])
         g_show_transfer_function_pressed = false;
     }
 
-    auto t = win.getTime();
     auto size = win.windowSize();
     glViewport(0, 0, size.x, size.y);
     //glClearColor(0.2f, .2f, .2f, 1.0f);
@@ -135,10 +192,15 @@ int main(int argc, char* argv[])
 
     glm::vec3 translate = max_volume_bounds * glm::vec3(-0.5f);
 
-    glm::vec3 eye = glm::vec3(2.0f*std::sin(t), 0, 2.0f*std::cos(t));
+    glm::vec3 eye = glm::vec3(0.0f, 0.0f, 2.0f);
     glm::vec3 target = glm::vec3(0.0f);
     glm::vec3 up(0.0f, 1.0f, 0.0f);
-    auto model_view = glm::lookAt(eye, target, up) * glm::translate(translate);
+    auto model_view = glm::lookAt(eye, target, up)
+                    * manipulator.matrix(win)
+                    * glm::rotate(0.5f*float(M_PI), glm::vec3(0.0f,1.0f,0.0f))
+                    * glm::rotate(0.5f*float(M_PI), glm::vec3(1.0f,0.0f,0.0f))
+                    * glm::translate(translate)
+                    ;
 
     glm::vec4 camera_translate = glm::column(glm::inverse(model_view), 3);
     glm::vec3 camera_location = glm::vec3(camera_translate.x, camera_translate.y, camera_translate.z);
